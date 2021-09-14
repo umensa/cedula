a8db11ed531f1a1e4a98649c70a783f5df5c2abe
umensa <mensae@posteo.org>
2021-09-15 00:33:47 +0100

    create views

--- db/migrate/20210914223329_create_participants.rb
+++ db/migrate/20210914223329_create_participants.rb
@@ -2,7 +2,7 @@ class CreateParticipants < ActiveRecord::Migration[6.1]
   def change
     create_table :participants do |t|
       t.string :participant_name
-      t.references :session, null: false, foreign_key: true
+      t.references :session, null: true, foreign_key: true
 
       t.timestamps
     end
