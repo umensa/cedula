549953ed683080b29fb6079c0f4b6f9c28a37b63
umensa <mensae@posteo.org>
2021-09-19 17:16:45 +0100

    test mentor model

--- spec/models/mentor_spec.rb
+++ spec/models/mentor_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Mentor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) {
+    Mentor.create(
+      mentor_name: "John"
+    )
+  }
+
+  it "is valid with valid attribtes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a mentor_name" do
+    expect(subject.mentor_name = "").to be_empty
+    expect(subject).to_not be_valid
+
+    subject.mentor_name = nil
+    expect(subject).to_not be_valid
+  end
 end
